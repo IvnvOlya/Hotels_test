f3b959445ddcdd84aa94c5117d98bada20ca49d3
Olga Ivanova <60544168+IvnvOlya@users.noreply.github.com>
2022-04-11 19:06:52 +0300

    first commit

--- src/Component/AuthorizationPage/AuthorizationPage.js
+++ src/Component/AuthorizationPage/AuthorizationPage.js
@@ -29,7 +29,7 @@ export default function AuthorizationPage() {
   const loginHandler = (e) => {
     setLogin(e.target.value);
     const re =
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!re.test(String(e.target.value).toLowerCase())) {
       setLoginErrors("Некорректный логин");
--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import ReactDOM from "react-dom/client"
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './Component/App/App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(<App/>)
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
